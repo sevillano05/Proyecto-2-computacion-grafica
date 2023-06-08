393ed7dd4ecf3047028edaf09aee914ced050774
antonio <antoniosevillano05@gmail.com>
2023-06-08 17:36:39 -0500

    agregado wither y steves a la escena

--- public/client.js
+++ public/client.js
@@ -29,7 +29,8 @@ function init(){
     scene=new THREE.Scene();
 
     camera=new THREE.PerspectiveCamera(40,window.innerWidth/window.innerHeight,0.05,10000)
-    camera.position.set(0,400,700)
+    
+    camera.position.set(-50,0,-300)
 
     controls=new OrbitControls(camera,renderer.domElement)
 
@@ -75,22 +76,21 @@ function init(){
     // base
     const mtlLoaderBase = new MTLLoader()
     const objLoaderBase = new OBJLoader()
-
     mtlLoaderBase.load('assets/models/nether/Nether.mtl',//////////////////////////////////mtl
-        (baseMTL) => {
-            baseMTL.preload()
-            for (const material of Object.values(baseMTL.materials)) {
+        (mtl) => {
+            mtl.preload()
+            for (const material of Object.values(mtl.materials)) {
                 material.side = THREE.DoubleSide
                 //material.map=baseMTL
               }
-            console.log(baseMTL)
-            objLoaderBase.setMaterials(baseMTL)
+            console.log(mtl)
+            objLoaderBase.setMaterials(mtl)
             objLoaderBase.load('assets/models/nether/Nether.obj',////////////////////////////////////obj
                 (base) => {
                     base.traverse((child) => {
                         child.scale.multiplyScalar(10)
                     });
-                    base.position.set(0, -380, 0)
+                    base.position.set(100, -380, 0)
                     scene.add(base)
                 },
                 (xhr) => {
@@ -110,25 +110,131 @@ function init(){
     )
 
 
-    //mobs
+    /////////////////////////MOBS
+    //wither
     const mtlLoaderBoss = new MTLLoader()
     const objLoaderBoss = new OBJLoader()
-    mtlLoaderBoss.load('assets/models/wither skeleton/wither+skeleton.mtl',//////////////////////////////////mtl
-        (bossMTL) => {
-            bossMTL.preload()
-            for (const material of Object.values(bossMTL.materials)) {
+    mtlLoaderBoss.load('assets/models/wither/wither.mtl',//////////////////////////////////mtl
+        (mtl) => {
+            mtl.preload()
+            for (const material of Object.values(mtl.materials)) {
                 material.side = THREE.DoubleSide
-                //material.map=bossMTL
               }
-            console.log(bossMTL)
-            objLoaderBoss.setMaterials(bossMTL)
-            objLoaderBoss.load('assets/models/wither skeleton/wither+skeleton.obj',////////////////////////////////////obj
-                (boss) => {
-                    boss.traverse((child) => {
+            console.log(mtl)
+            objLoaderBoss.setMaterials(mtl)
+            objLoaderBoss.load('assets/models/wither/wither.obj',////////////////////////////////////obj
+                (obj) => {
+                    obj.traverse((child) => {
+                        child.scale.multiplyScalar(4)
+                    });
+                    obj.position.set(-50, -100, 700)
+                    obj.rotation.y=Math.PI/1
+                    scene.add(obj)
+                },
+                (xhr) => {
+                    console.log((xhr.loaded / xhr.total) * 100 + '% loaded')
+                },
+                (error) => {
+                    console.log('An error happened')
+                }
+            )
+        },
+        (xhr) => {
+            console.log((xhr.loaded / xhr.total) * 100 + '% loaded')
+        },
+        (error) => {
+            console.log('An error happened')
+        }
+    )
+
+    //wither skeleton
+    const mtlLoaderWitherSkeleton = new MTLLoader()
+    const objLoaderWitherSkeleton = new OBJLoader()
+    mtlLoaderWitherSkeleton.load('assets/models/wither skeleton/wither+skeleton.mtl',//////////////////////////////////mtl
+        (mtl) => {
+            mtl.preload()
+            for (const material of Object.values(mtl.materials)) {
+                material.side = THREE.DoubleSide
+              }
+            console.log(mtl)
+            objLoaderWitherSkeleton.setMaterials(mtl)
+            objLoaderWitherSkeleton.load('assets/models/wither skeleton/wither+skeleton.obj',////////////////////////////////////obj
+                (obj) => {
+                    obj.traverse((child) => {
                         child.scale.multiplyScalar(3)
                     });
-                    boss.position.set(0, 0, 0)
-                    scene.add(boss)
+                    obj.position.set(0, 0, 0)
+                    scene.add(obj)
+                },
+                (xhr) => {
+                    console.log((xhr.loaded / xhr.total) * 100 + '% loaded')
+                },
+                (error) => {
+                    console.log('An error happened')
+                }
+            )
+        },
+        (xhr) => {
+            console.log((xhr.loaded / xhr.total) * 100 + '% loaded')
+        },
+        (error) => {
+            console.log('An error happened')
+        }
+    )
+
+    //steve
+    const mtlLoaderSteve = new MTLLoader()
+    const objLoaderSteve = new OBJLoader()
+    mtlLoaderSteve.load('assets/models/minecraft simple rig/Minecraft_Simple_Rig.mtl',//////////////////////////////////mtl
+        (mtl) => {
+            mtl.preload()
+            for (const material of Object.values(mtl.materials)) {
+                material.side = THREE.DoubleSide
+              }
+            console.log(mtl)
+            objLoaderSteve.setMaterials(mtl)
+            objLoaderSteve.load('assets/models/minecraft simple rig/Minecraft_Simple_Rig.obj',////////////////////////////////////obj
+                (obj) => {
+                    obj.traverse((child) => {
+                        child.scale.multiplyScalar(4)
+                    });
+                    obj.position.set(-50, -15, -10)
+                    scene.add(obj)
+                },
+                (xhr) => {
+                    console.log((xhr.loaded / xhr.total) * 100 + '% loaded')
+                },
+                (error) => {
+                    console.log('An error happened')
+                }
+            )
+        },
+        (xhr) => {
+            console.log((xhr.loaded / xhr.total) * 100 + '% loaded')
+        },
+        (error) => {
+            console.log('An error happened')
+        }
+    )
+
+    //steve pro max
+    const mtlLoaderSteveBlank = new MTLLoader()
+    const objLoaderSteveBlank = new OBJLoader()
+    mtlLoaderSteveBlank.load('assets/models/3d model blank/3d-model.mtl',//////////////////////////////////mtl
+        (mtl) => {
+            mtl.preload()
+            for (const material of Object.values(mtl.materials)) {
+                material.side = THREE.DoubleSide
+              }
+            console.log(mtl)
+            objLoaderSteveBlank.setMaterials(mtl)
+            objLoaderSteveBlank.load('assets/models/3d model blank/3d-model.obj',////////////////////////////////////obj
+                (obj) => {
+                    obj.traverse((child) => {
+                        child.scale.multiplyScalar(0.13)
+                    });
+                    obj.position.set(-90, -10, 50)
+                    scene.add(obj)
                 },
                 (xhr) => {
                     console.log((xhr.loaded / xhr.total) * 100 + '% loaded')
