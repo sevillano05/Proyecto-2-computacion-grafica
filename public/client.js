bed6141618592f64fda4d84884b5f8545557a65f
antonio <antoniosevillano05@gmail.com>
2023-06-08 23:13:49 -0500

    cambios al nether, añadido:angel,espada

--- public/client.js
+++ public/client.js
@@ -3,6 +3,8 @@ import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
 import { OBJLoader } from './jsm/loaders/OBJLoader.js';
 import { MTLLoader } from './jsm/loaders/MTLLoader.js';
 
+
+
 let camera, scene, renderer,controls;
 
 
@@ -31,8 +33,11 @@ function init(){
     camera=new THREE.PerspectiveCamera(40,window.innerWidth/window.innerHeight,0.05,10000)
     
     camera.position.set(-50,0,-300)
+    
 
     controls=new OrbitControls(camera,renderer.domElement)
+
+    controls.target.set(-50,0,0)
 
 
     ////////////////////////////////////////////HELPER//////////////////////////////////////
@@ -45,12 +50,22 @@ function init(){
     /////////////////////////////////////////////////////////////////////////////////////////////
 
     //light
-    const sunlight = new THREE.AmbientLight(0xFFffff,4)
-    scene.add(sunlight)
+    const light = new THREE.AmbientLight(0xFFffff,3)
+    scene.add(light)
 
+    //contrapicado
+    const lavaLight1 = new THREE.PointLight(0xff9999,50,2000)
+    lavaLight1.position.set(-50,-150,300)
+    lavaLight1.castShadow=true
+    scene.add(lavaLight1)
 
+    // const lavaLight2 = new THREE.PointLight(0xff9999,50,2000)
+    // lavaLight2.position.set(350,-150,-200)
+    // lavaLight2.castShadow=true
+    // scene.add(lavaLight2)
 
-    // Add Sky
+    
+    ///////////////// Add Sky
     const skyBox = new THREE.SphereGeometry(1500,25,25)
     const skyTex = new THREE.TextureLoader().load('assets/slava.jpg')
     const skyMat = new THREE.MeshBasicMaterial({
@@ -60,20 +75,8 @@ function init(){
     scene.add(sky)
 
 
-    //piso
-    /*const floorGeo = new THREE.BoxGeometry(1000, 1000, 10); 
-    const floorTex = new THREE.TextureLoader().load('assets/lava.png')
-    floorTex.wrapS=floorTex.wrapT = THREE.RepeatWrapping;
-    floorTex.repeat.set(15, 15);
-    const floorMat = new THREE.MeshStandardMaterial( {color: 0xFFFFFF,map:floorTex,} );
-    const floor = new THREE.Mesh(floorGeo, floorMat)
-    floor.rotateX (THREE.MathUtils.degToRad(90))
-    floor.position.set(0,-4,0)
-    floor.receiveShadow=true
-    scene.add(floor)*/
-
-
-    // base
+    
+    ///////////////////// base
     const mtlLoaderBase = new MTLLoader()
     const objLoaderBase = new OBJLoader()
     mtlLoaderBase.load('assets/models/nether/Nether.mtl',//////////////////////////////////mtl
@@ -81,7 +84,6 @@ function init(){
             mtl.preload()
             for (const material of Object.values(mtl.materials)) {
                 material.side = THREE.DoubleSide
-                //material.map=baseMTL
               }
             console.log(mtl)
             objLoaderBase.setMaterials(mtl)
@@ -89,6 +91,7 @@ function init(){
                 (base) => {
                     base.traverse((child) => {
                         child.scale.multiplyScalar(10)
+                        child.castShadow = child.receiveShadow = true;
                     });
                     base.position.set(100, -380, 0)
                     scene.add(base)
@@ -126,6 +129,7 @@ function init(){
                 (obj) => {
                     obj.traverse((child) => {
                         child.scale.multiplyScalar(4)
+                        child.castShadow = child.receiveShadow = true
                     });
                     obj.position.set(-50, -100, 700)
                     obj.rotation.y=Math.PI/1
@@ -163,7 +167,112 @@ function init(){
                     obj.traverse((child) => {
                         child.scale.multiplyScalar(3)
                     });
-                    obj.position.set(0, 0, 0)
+                    obj.position.set(885, 140, -200)
+                    obj.rotation.y=Math.PI/6
+                    scene.add(obj)
+                },
+                (xhr) => {
+                    console.log((xhr.loaded / xhr.total) * 100 + '% loaded')
+                },
+                (error) => {
+                    console.log('An error happened')
+                }
+            )
+        },
+        (xhr) => {
+            console.log((xhr.loaded / xhr.total) * 100 + '% loaded')
+        },
+        (error) => {
+            console.log('An error happened')
+        }
+    )
+
+    const mtlLoaderWitherSkeleton1 = new MTLLoader()
+    const objLoaderWitherSkeleton1 = new OBJLoader()
+    mtlLoaderWitherSkeleton1.load('assets/models/wither skeleton/wither+skeleton.mtl',//////////////////////////////////mtl
+        (mtl) => {
+            mtl.preload()
+            for (const material of Object.values(mtl.materials)) {
+                material.side = THREE.DoubleSide
+              }
+            console.log(mtl)
+            objLoaderWitherSkeleton1.setMaterials(mtl)
+            objLoaderWitherSkeleton1.load('assets/models/wither skeleton/wither+skeleton.obj',////////////////////////////////////obj
+                (obj) => {
+                    obj.traverse((child) => {
+                        child.scale.multiplyScalar(3)
+                    });
+                    obj.position.set(250, 65, -15)
+                    obj.rotation.y=Math.PI/4
+                    scene.add(obj)
+                },
+                (xhr) => {
+                    console.log((xhr.loaded / xhr.total) * 100 + '% loaded')
+                },
+                (error) => {
+                    console.log('An error happened')
+                }
+            )
+        },
+        (xhr) => {
+            console.log((xhr.loaded / xhr.total) * 100 + '% loaded')
+        },
+        (error) => {
+            console.log('An error happened')
+        }
+    )
+
+    const mtlLoaderWitherSkeleton2 = new MTLLoader()
+    const objLoaderWitherSkeleton2 = new OBJLoader()
+    mtlLoaderWitherSkeleton2.load('assets/models/wither skeleton/wither+skeleton.mtl',//////////////////////////////////mtl
+        (mtl) => {
+            mtl.preload()
+            for (const material of Object.values(mtl.materials)) {
+                material.side = THREE.DoubleSide
+              }
+            console.log(mtl)
+            objLoaderWitherSkeleton2.setMaterials(mtl)
+            objLoaderWitherSkeleton2.load('assets/models/wither skeleton/wither+skeleton.obj',////////////////////////////////////obj
+                (obj) => {
+                    obj.traverse((child) => {
+                        child.scale.multiplyScalar(3)
+                    });
+                    obj.position.set(750, -165, 150)
+                    obj.rotation.y=Math.PI/2
+                    scene.add(obj)
+                },
+                (xhr) => {
+                    console.log((xhr.loaded / xhr.total) * 100 + '% loaded')
+                },
+                (error) => {
+                    console.log('An error happened')
+                }
+            )
+        },
+        (xhr) => {
+            console.log((xhr.loaded / xhr.total) * 100 + '% loaded')
+        },
+        (error) => {
+            console.log('An error happened')
+        }
+    )
+
+    const mtlLoaderWitherSkeleton3 = new MTLLoader()
+    const objLoaderWitherSkeleton3 = new OBJLoader()
+    mtlLoaderWitherSkeleton3.load('assets/models/wither skeleton/wither+skeleton.mtl',//////////////////////////////////mtl
+        (mtl) => {
+            mtl.preload()
+            for (const material of Object.values(mtl.materials)) {
+                material.side = THREE.DoubleSide
+              }
+            console.log(mtl)
+            objLoaderWitherSkeleton3.setMaterials(mtl)
+            objLoaderWitherSkeleton3.load('assets/models/wither skeleton/wither+skeleton.obj',////////////////////////////////////obj
+                (obj) => {
+                    obj.traverse((child) => {
+                        child.scale.multiplyScalar(3)
+                    });
+                    obj.position.set(-450, 385, -300)
                     scene.add(obj)
                 },
                 (xhr) => {
@@ -197,8 +306,10 @@ function init(){
                 (obj) => {
                     obj.traverse((child) => {
                         child.scale.multiplyScalar(4)
+                        child.castShadow = child.receiveShadow = true;
                     });
-                    obj.position.set(-50, -15, -10)
+                    obj.position.set(-80, -5, 0)
+                    obj.rotation.x=THREE.MathUtils.degToRad(90);
                     scene.add(obj)
                 },
                 (xhr) => {
@@ -232,8 +343,9 @@ function init(){
                 (obj) => {
                     obj.traverse((child) => {
                         child.scale.multiplyScalar(0.13)
+                        child.castShadow = child.receiveShadow = true;
                     });
-                    obj.position.set(-90, -10, 50)
+                    obj.position.set(-100, -10, 80)
                     scene.add(obj)
                 },
                 (xhr) => {
@@ -252,28 +364,83 @@ function init(){
         }
     )
 
+    //espada
+    const mtlLoaderEspada = new MTLLoader()
+    const objLoaderEspada = new OBJLoader()
+    mtlLoaderEspada.load('assets/models/diamond sword/sword.mtl',//////////////////////////////////mtl
+        (mtl) => {
+            mtl.preload()
+            for (const material of Object.values(mtl.materials)) {
+                material.side = THREE.DoubleSide
+              }
+            console.log(mtl)
+            objLoaderEspada.setMaterials(mtl)
+            objLoaderEspada.load('assets/models/diamond sword/sword.obj',////////////////////////////////////obj
+                (obj) => {
+                    obj.traverse((child) => {
+                        child.scale.multiplyScalar(0.4)
+                        child.castShadow = child.receiveShadow = true;
+                    });
+                    obj.position.set(-100, 0, 20)
+                    obj.rotation.x=THREE.MathUtils.degToRad(125);
+                    scene.add(obj)
+                },
+                (xhr) => {
+                    console.log((xhr.loaded / xhr.total) * 100 + '% loaded')
+                },
+                (error) => {
+                    console.log('An error happened')
+                }
+            )
+        },
+        (xhr) => {
+            console.log((xhr.loaded / xhr.total) * 100 + '% loaded')
+        },
+        (error) => {
+            console.log('An error happened')
+        }
+    )  
 
-    /*const baseTex = new THREE.TextureLoader().load('assets/stone.jpg');
-    baseTex.wrapS = baseTex.wrapT = THREE.RepeatWrapping;
-    baseTex.repeat.set(2, 2);
-    const baseMat = new THREE.MeshPhongMaterial({map: baseTex});
-
-    const objLoaderWicked = new OBJLoader()
-    objLoaderWicked.load( 'assets/models/minecraft.obj', ( Wicked ) => {
-        Wicked.traverse(( child ) => {
-            //child.material = baseMat // Asignar el material al mesh del objeto
-            child.scale.multiplyScalar(1)
-            child.position.set(10, 0, 0) 
-        } );
-    scene.add( Wicked );
-    } );*/
-    
-
-    
+    //angel(?)
+    const mtlLoaderAngel = new MTLLoader()
+    const objLoaderAngel = new OBJLoader()
+    mtlLoaderAngel.load('assets/models/angel/untitled.mtl',//////////////////////////////////mtl
+        (mtl) => {
+            mtl.preload()
+            for (const material of Object.values(mtl.materials)) {
+                material.side = THREE.DoubleSide
+              }
+            console.log(mtl)
+            objLoaderAngel.setMaterials(mtl)
+            objLoaderAngel.load('assets/models/angel/untitled.obj',////////////////////////////////////obj
+                (obj) => {
+                    obj.traverse((child) => {
+                        child.scale.multiplyScalar(3)
+                    });
+                    obj.position.set(-80, 10, 20)
+                    obj.rotation.y=THREE.MathUtils.degToRad(180);
+                    scene.add(obj)
+                },
+                (xhr) => {
+                    console.log((xhr.loaded / xhr.total) * 100 + '% loaded')
+                },
+                (error) => {
+                    console.log('An error happened')
+                }
+            )
+        },
+        (xhr) => {
+            console.log((xhr.loaded / xhr.total) * 100 + '% loaded')
+        },
+        (error) => {
+            console.log('An error happened')
+        }
+    )  
     
 
 ///////////////////////////////////////////////////////////////////////////
     window.addEventListener( 'resize', onWindowResize );
+    
 }
 
 function onWindowResize() {
@@ -289,7 +456,11 @@ function onWindowResize() {
 function animate() {
     requestAnimationFrame(animate);
     renderer.render(scene, camera)
+    renderer.shadowMap.enabled = true;
+    renderer.shadowMap.type = THREE.PCFSoftShadowMap;
+    
     controls.update()
+    
 }
 
 
